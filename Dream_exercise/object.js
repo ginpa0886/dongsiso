55565db61aae05e5ae68ded113b71015ba258a2e
Gomdi <ginpa0886@gmail.com>
2021-01-21 16:31:29 +0900

    2021.01.21(목)

--- Dream_exercise/object.js
+++ Dream_exercise/object.js
@@ -17,9 +17,10 @@ class Person{
 
 
 const dongwon = new Person('dongwon', 20)
-console.log(dongwon.name);
-console.log(dongwon.age);
-dongwon.speak();
+// console.log(dongwon);
+// console.log(dongwon.name);
+// console.log(dongwon.age);
+// dongwon.speak();
 
 // 2. Getter and setters
 class User{
@@ -28,4 +29,87 @@ class User{
     this.lastName = lastName;
     this.age = age;
   }
+
+  get age() {
+    return this._age;
+  }
+
+  set age(value) {
+		// if(value < 0){
+		// 	throw Error('age can not be negative');
+		// }
+    this._age = value < 0 ? 0 : value;
+  }
 }
+
+const user1 = new User('Steve', 'Job', -1);
+// console.log(user1);
+// console.log(user1.age);
+
+
+// 3. Fields (public, private)
+class Experiment {
+  publicFields = 2;
+  #privateField = 0;
+}
+const experiment = new Experiment();
+// console.log(experiment.publicFields);
+// console.log(experiment.privateField);
+
+// 4. Static properties and methods
+class Article {
+  static publisher = 'Dream Coding';
+  constructor(articleNumber) {
+    this.articleNumber = articleNumber;
+  }
+
+  static printPublisher() {
+    console.log(Article.publisher);
+  }
+}
+
+const article1 = new Article(1);
+const aritcle2 = new Article(2);
+// console.log(article1.publisher);
+// console.log(aritcle2.publisher);
+// console.log(Article.publisher);
+
+
+// 5. Inheritance
+class Shape {
+  constructor(width, height, color) {
+    this.width = width;
+    this.height = height;
+    this.color = color;
+  }
+
+  draw() {
+    console.log(`drawing ${this.color} color of`);
+  }
+
+  getArea() {
+    return width * this.height;
+  }
+}
+
+class Rectangle extends Shape {}
+class Triangle extends Shape {
+  draw() {
+    super.draw();
+    console.log('∆');
+  }
+}
+
+const rectangle = new Rectangle(20, 20, 'blue');
+const trinagle = new Triangle()
+// console.log(rectangle);
+// trinagle.draw()
+// rectangle.draw();
+
+
+// 6. Class checking: instanceOf
+console.log(rectangle instanceof Rectangle);
+console.log(trinagle instanceof Rectangle);
+console.log(trinagle instanceof Triangle);
+console.log(trinagle instanceof Shape);
+console.log(trinagle instanceof Object);
---
+++ Kimbug_mento/React/dong_app_2021/src/components/Movie.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import ProrpTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import "./Movie.css";
+
+function Movie({id, year, title, summary, poster, genres }) {
+  return (
+      <div className="movie">
+        <Link to= {{
+            pathname: `movie/${id}`,
+            state: {
+              year,
+              title,
+              summary,
+              poster,
+              genres
+            }
+          }}>
+          <img src={poster} alt={title} title={title} />
+          <div className="movie__data">
+          <h3 className="movie__title">{title}</h3>
+          <h5 className="movie__year">{year}</h5>
+          <ul className="movie__genres">
+            {genres.map((genre, index) => (
+              <li key={index} className="genres__genre">{genre}</li>
+            ))}
+          </ul>
+          <p className="movie__summary">{summary.slice(0, 180)}...</p>
+        </div>
+      </Link>
+    </div>
+    )
+}
+
+Movie.prorpTypes = {
+  id: ProrpTypes.number.isRequired,
+  year: ProrpTypes.number.isRequired,
+  title: ProrpTypes.string.isRequired,
+  summary: ProrpTypes.string.isRequired,
+  poster: ProrpTypes.string.isRequired,
+  genres: ProrpTypes.arrayOf(ProrpTypes.string).isRequired
+};
+
+
+
+export default Movie;
