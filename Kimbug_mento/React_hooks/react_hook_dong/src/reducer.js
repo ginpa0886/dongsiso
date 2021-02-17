3404898ad1d0a7c0d89cb262352f70678b78928b
Gomdi <ginpa0886@gmail.com>
2021-02-17 18:39:21 +0900

    react_hook_practice

--- Kimbug_mento/React_hooks/react_hook_dong/src/reducer.js
+++ Kimbug_mento/React_hooks/react_hook_dong/src/reducer.js
@@ -1,20 +1,17 @@
 
 import { v4 as uuid } from 'uuid'
-
+import { ADD, DEL, COMPLETE, UNCOMPLETE } from "./actions";
 
 export const initialState = {
   toDos: [],
   completed: []
 }
 
-export const ADD = "increment";
-export const DEL = "delete";
-export const COMPLETE = 'complete'
-
 const reducer = (state, action) => {
   
   switch (action.type) {
     case ADD:
+      console.log({...state});
       return { ...state, toDos: [...state.toDos, { text: action.payload , id: uuid() }] };
     case DEL:
       return { ...state, toDos: state.toDos.filter(toDo => {
@@ -27,6 +24,13 @@ const reducer = (state, action) => {
       return { ...state, toDos: state.toDos.filter(toDo => {
         return toDo.id !== action.payload}
         ), completed: [...state.completed, {...target}]}
+    case UNCOMPLETE:
+      const atarget = state.completed.find(toDo => {
+        return toDo.id === action.payload}
+        )
+      return { ...state, completed: state.completed.filter(toDo => {
+        return toDo.id !== action.payload}
+        ), toDos: [...state.toDos, {...atarget}]}
     default:
       return;
   }
