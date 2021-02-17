0faec79ace430635b4b75c4ccf0462664e4426ee
Gomdi <ginpa0886@gmail.com>
2021-02-17 15:37:53 +0900

    Completing To DOs

--- Kimbug_mento/React_hooks/react_hook_dong/src/App.js
+++ Kimbug_mento/React_hooks/react_hook_dong/src/App.js
@@ -1,26 +1,60 @@
-import React, { useReducer } from "react";
+import React, { useReducer, useState } from "react";
 
-const INCREMENT = "increment";
-const DECREMENT = "decrement";
+import reducer, { initialState, ADD, DEL, COMPLETE } from './reducer'
 
-const reducer = (state, action) => {
-  switch (action.type) {
-    case INCREMENT:
-      return { count: state.count + 1 };
-    case DECREMENT:
-      return { count: state.count - 1 };
-    default:
-      throw new Error();
-  }
-};
+
 
 function App() {
-  const [state, dispatch] = useReducer(reducer, { count: 0 });
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const [newToDo, setNewToDo] = useState("");
+  const onSubmit = e => {
+    e.preventDefault();
+    dispatch({ type: ADD, payload: newToDo });
+    setNewToDo("")
+  };
+  const onChange = e => {
+    // console.log(e);
+    const {
+      target: { value }
+    } = e;
+    setNewToDo(value);
+  };
   return (
     <>
-      <h1>{state.count}</h1>
-      <button onClick={() => dispatch({ type: INCREMENT })}>Add</button>
-      <button onClick={() => dispatch({ type: DECREMENT })}>Remove</button>
+      <h1>Add to do </h1>
+      <form onSubmit={onSubmit}>
+        <input
+          value={newToDo}
+          type="text"
+          placeholder="Write to do"
+          onChange={onChange}
+        />
+      </form>
+
+      <ul>
+        <h2>To Dos</h2>
+        {state.toDos.map((toDo) => (
+          <li key={toDo.id}>
+            <span>{toDo.text}</span>
+            <button onClick={() => dispatch({ type: DEL, payload: toDo.id})}>✖</button>
+            <button onClick={() => dispatch({ type: DEL, payload: toDo.id})}>✔</button>
+            </li>
+        ))}
+      </ul>
+      <ul>
+        {state.completed.length !== 0 && (
+          <>
+            <h2>Completed</h2>
+            {state.completed.map((toDo) => (
+              <li key={toDo.id}>
+                <span>{toDo.text}</span>
+                <button onClick={() => dispatch({ type: COMPLETE, payload: toDo.id})}>✖</button>
+                <button onClick={() => dispatch({ type: COMPLETE, payload: toDo.id})}>🤔</button>
+                </li>
+            ))}
+          </>
+          )}
+      </ul>
     </>
   );
 }
